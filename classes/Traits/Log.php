fcbc4db752fc05337fe269d7eef851c74d1a917a
Justin Navarro <info@justinnavarro.net>
2019-05-14 19:46:08 -0400

    add info log

--- classes/Traits/Log.php
+++ classes/Traits/Log.php
@@ -9,15 +9,18 @@ trait Log
 {
     private $debug;
     private $error;
+    private $info;
     private $warning;
 
     protected function logSetup()
     {
         $this->debug = new Logger('DEV');
         $this->error = new Logger('DEV');
+        $this->info = new Logger('DEV');
         $this->warning = new Logger('DEV');
         $this->debug->pushHandler(new StreamHandler(__DIR__ . '/../../logs/debug.log', Logger::DEBUG));
         $this->error->pushHandler(new StreamHandler(__DIR__ . '/../../logs/error.log', Logger::ERROR));
+        $this->info->pushHandler(new StreamHandler(__DIR__ . '/../../logs/info.log', Logger::INFO));
         $this->warning->pushHandler(new StreamHandler(__DIR__ . '/../../logs/warning.log', Logger::WARNING));
     }
 
@@ -29,6 +32,11 @@ trait Log
     protected function error(string $message, array $details = [])
     {
         $this->error->error($message, $details);
+    }
+
+    protected function info(string $message, array $details = [])
+    {
+        $this->info->info($message, $details);
     }
 
     protected function warning(string $message, array $details = [])
