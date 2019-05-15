9f14d1ce4f6bd73ca6b7d0aacf47db781f075ec0
Justin Navarro <info@justinnavarro.net>
2019-05-15 15:59:21 -0400

    update the logic to grab 20 users at a time (since this call is tier 2 and can only take 20 requests/min), loop to keep grabbing another 20 users every min and deactivate any user that meets the criteria specified

--- deactivate_users.php
+++ deactivate_users.php
@@ -5,13 +5,29 @@ require_once __DIR__ . '/bootstrap.php';
 use JoliCode\Slack\Api\Model\ObjsUser;
 
 $usersObj = new Src\Controllers\Users;
-$users = $usersObj->getAllUsers([ 'limit' => 6 ]);
+$options = [ 'limit' => 20 ];
+$users = $usersObj->getAllUsers($options);
 $whitelist = [];
 
-array_map(function(ObjsUser $user) use ($usersObj, $whitelist) {
-    $email = $usersObj->getUserEmail($user);
+$i = 0;
+while($users) {
+    array_map(function(ObjsUser $user) use ($usersObj, $whitelist) {
+        $email = $usersObj->getUserEmail($user);
 
-    if (!$user->getDeleted() && $usersObj->isUserAMember($user) && !$usersObj->isBillingActive($user) && !in_array($email, $whitelist)) {
-        $usersObj->setUserAsDeactivated($user);
+        if (!$user->getDeleted() && $usersObj->isUserAMember($user) && !$usersObj->isBillingActive($user) && !in_array($email, $whitelist) && !$user->getIsBot()) {
+            $usersObj->setUserAsDeactivated($user);
+        } else {
+            $usersObj->info('User is skipped', [ 'user' => $user->getRealName(), 'email' => $email ]);
+        }
+    }, $users);
+
+    $options['cursor'] = $usersObj->getNextCursor();
+    $users = $usersObj->getAllUsers($options);
+
+    sleep(60);
+    $i++;
+
+    if ($i === 900) {
+        $users = [];
     }
-}, $users);
+}
